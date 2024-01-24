82960a33b0e2505eed1b6590af88df057c257169
Green Gem <chris_waitt@me.com>
2024-01-24 19:43:54 +0000

    initial idea for starting a game, needs search functionality with bgg and an updated schema

---
+++ app/play/page.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+
+export default function PlayPage() {
+    const gameId = '367150';
+
+    return(
+        <>
+            <h1>Play Page</h1>
+
+            <h4>What are you playing?</h4>            
+            <input type="search" defaultValue="Search for a game" className="block" />
+
+            <h4>Who is playing?</h4>
+            <input type="search" defaultValue="Search for a game" className="block" />
+
+            <Link href={`/play/${gameId}`} className="bg-blue-500 p-2 block">Play now</Link>
+
+        </>
+    )
+}
