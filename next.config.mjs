54755f5349788583d5fba91c66353b1cfa8ff9cf
Green Gem <chris_waitt@me.com>
2024-01-24 19:42:39 +0000

    installed xml2js to parse xml returned from BGG

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      domains: ['cf.geekdo-images.com'],
+    },
+  };
+  
+  export default nextConfig;
+  
