0f68d766f183337f1af8dbd86810aab333c4f894
Green Gem <chris_waitt@me.com>
2024-02-01 18:44:59 +0000

    fetch more data for individual board games

--- data/FetchBoardGameData.tsx
+++ data/FetchBoardGameData.tsx
@@ -17,16 +17,26 @@ export default async function fetchBoardGameData(gameId: string) {
     const xmlData = await res.text();
     const jsonData = await parseStringPromise(xmlData);
     const gameDetails = jsonData.boardgames.boardgame[0];
+    //console.log(gameDetails);
 
     const primaryNameEntry = gameDetails.name.find((name: NameEntry) => name.$ && name.$.primary === 'true');
     const primaryName = primaryNameEntry ? primaryNameEntry._ : 'Name not found';
 
     const game = {
-        id: gameDetails.$.objectid,
-        name: primaryName,
-        image: gameDetails.image[0],
-        thumbnail: gameDetails.thumbnail[0],
-    };
+      id: gameDetails.$.objectid,
+      name: primaryName,
+      image: gameDetails.image[0],
+      thumbnail: gameDetails.thumbnail[0],
+      yearPublished: gameDetails.yearpublished[0],
+      minPlayers: gameDetails.minplayers[0],
+      maxPlayers: gameDetails.maxplayers[0],
+      playingTime: gameDetails.playingtime[0],
+      minPlayTime: gameDetails.minplaytime[0],
+      maxPlayTime: gameDetails.maxplaytime[0],
+      age: gameDetails.age[0],
+      description: gameDetails.description[0],
+  };
+  
     
     return game;
 }
