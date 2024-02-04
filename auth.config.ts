d8fd044846be793adb9d7b47c842f72487ee62b9
Green Gem <chris_waitt@me.com>
2024-02-04 12:23:25 +0000

    removed custom signin redirect

--- auth.config.ts
+++ auth.config.ts
@@ -3,9 +3,6 @@ import GitHub from "next-auth/providers/github";
 import type { NextAuthConfig } from "next-auth";
 
 export default {
-  pages: {
-    signIn: '/login',
-  },
   providers: [
     GitHub({
       clientId: process.env.GITHUB_ID,
