09580810ae7fa97dc4e369883928f57ae9c853e1
Green Gem <chris_waitt@me.com>
2024-02-04 13:06:05 +0000

    cleaned up navbar

--- ui/Navbar.tsx
+++ ui/Navbar.tsx
@@ -1,19 +1,11 @@
 import Link from "next/link";
-
+import { Button } from "@nextui-org/button";
 export default function Navbar() {
     return(
         <nav className="flex justify-between items-center bg-black px-5 py-3 text-white">
             <h4 className="text-xl tracking-tight"><Link href='/'>Brand</Link></h4>
             <ul className="flex gap-3">
-                <li>
-                    <Link href='/play'>Play</Link>
-                </li>
-                <li>
-                    <Link href='#'>Link</Link>
-                </li>
-                <li>
-                    <Link href='#'>Link</Link>
-                </li>
+                <li><Button color="primary" size="sm" as={Link} href='/new-game'>New Game</Button></li>
             </ul>
         </nav>
     )
