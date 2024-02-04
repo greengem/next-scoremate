685fc501b76c62d08b6682835d8ec1c88aed3f48
Green Gem <chris_waitt@me.com>
2024-02-04 12:27:39 +0000

    removed old files

--- app/play/components/BoardGameSearchResults.tsx
+++ app/play/components/BoardGameSearchResults.tsx
@@ -1,12 +1,16 @@
 import fetchBoardGameSearch from "@/data/FetchBoardGameSearch";
-import BoardGameSearchResultsTable from "./BoardGameSearchResultsTable";
+import Link from "next/link";
 
 export default async function BoardGameSearchResults({ query } : { query: string }) {
     const searchResults = await fetchBoardGameSearch(query);
     return (
         <ul>
             {searchResults.map((searchItem: any) => (
-                <li key={searchItem.id}>{searchItem.name}</li>
+                <li key={searchItem.id}>
+                    <Link href={`/play/${searchItem.id}`}>
+                        {searchItem.name}
+                    </Link>
+                </li>
             ))}
         </ul>
     )
