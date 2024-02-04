c1ec813826e654a3b7dc66aac64d44dd3fdb7b38
Green Gem <chris_waitt@me.com>
2024-02-04 12:12:52 +0000

    Remove table from search results

--- app/play/components/BoardGameSearchResults.tsx
+++ app/play/components/BoardGameSearchResults.tsx
@@ -3,5 +3,11 @@ import BoardGameSearchResultsTable from "./BoardGameSearchResultsTable";
 
 export default async function BoardGameSearchResults({ query } : { query: string }) {
     const searchResults = await fetchBoardGameSearch(query);
-    return (<BoardGameSearchResultsTable searchResults={searchResults} />)
+    return (
+        <ul>
+            {searchResults.map((searchItem: any) => (
+                <li key={searchItem.id}>{searchItem.name}</li>
+            ))}
+        </ul>
+    )
 }
