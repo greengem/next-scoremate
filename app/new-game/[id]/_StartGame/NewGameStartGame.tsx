f25aa0d4c0aed4e35cebfe1e9568d77193827a32
Green Gem <chris_waitt@me.com>
2024-03-18 08:32:54 +0000

    Refactor NewGameStartGame component to use Next UI Card component

--- app/new-game/[id]/_StartGame/NewGameStartGame.tsx
+++ app/new-game/[id]/_StartGame/NewGameStartGame.tsx
@@ -4,20 +4,25 @@ import { Button } from "@nextui-org/button";
 import { Input } from "@nextui-org/input";
 import { handleStartGame } from "@/server-actions/handleStartGame";
 import { User } from "@nextui-org/user";
+import { Card, CardBody, CardHeader, CardFooter } from '@nextui-org/card';
 
 export default function NewGameStartGame({
     gameId,
-    creator
+    userId,
+    userName,
+    userAvatar
 }: {
     gameId: string;
-    creator: any;
+    userId: string;
+    userName: string;
+    userAvatar: string | undefined;
 }) {
-    const [players, setPlayers] = useState([{ id: creator.user.id, name: creator.user.name, avatar: creator.user.image, isGuest: false, userType: 'Me' }]);
+    const [players, setPlayers] = useState([{ id: userId, name: userName, avatar: userAvatar, isGuest: false, userType: 'Me' }]);
     const [guestName, setGuestName] = useState('');
     const [userType, setUserType] = useState('Guest');
 
     const addGuest = () => {
-        setPlayers([...players, { id: Math.random().toString(), name: guestName, avatar: 'guestAvatar.png', isGuest: true, userType }]);
+        setPlayers([...players, { id: Math.random().toString(), name: guestName, avatar: undefined, isGuest: true, userType }]);
         setGuestName('');
     };
 
@@ -27,38 +32,39 @@ export default function NewGameStartGame({
     };
 
     return (
-        <div className="space-y-5">
-            <div className="flex gap-2">
-                <div className="flex gap-2 items-center">
-                    <Button className="shrink-0" onClick={addGuest}>Add Guest</Button> 
-                    <Input className="shrink-0" labelPlacement="outside" placeholder="Guest name..." value={guestName} onChange={(e) => setGuestName(e.target.value)} />
-                    <select onChange={(e) => setUserType(e.target.value)} value={userType}>
-                        <option value="Guest">Guest</option>
-                        <option value="Friend">Friend</option>
-                    </select>
-                </div>
+        <Card className='max-w-96 mx-auto'>
+            <CardHeader>Players</CardHeader>
+            <CardBody>
+
+            <div className="flex gap-2 items-center mb-5">
+                <Input size='sm' className="grow" labelPlacement="outside" placeholder="Guest name..." value={guestName} onChange={(e) => setGuestName(e.target.value)} />
+                <Button size='sm' className="shrink-0" onClick={addGuest}>Add Guest</Button> 
+                <select onChange={(e) => setUserType(e.target.value)} value={userType} className='hidden'>
+                    <option value="Guest">Guest</option>
+                    <option value="Friend">Friend</option>
+                </select>
             </div>
 
-            <div>
-                <h2 className="text-lg font-semibold">Players</h2>
-                <div className='flex flex-col items-start space-y-3'>
-                    {players.map(player => (
-                            <User
-                                key={player.id}
-                                name={player.name}
-                                description={player.userType}
-                                avatarProps={{
-                                    src: player.avatar
-                                }}
-                            />
-                    ))}
-                </div>
-            </div>
 
-            <form onSubmit={startGame}>
-                <input type="hidden" name="gameId" value={gameId} />
-                <Button type="submit">Start Game</Button>
-            </form>
-        </div>
+            <div className='flex flex-col items-start space-y-3'>
+                {players.map(player => (
+                    <User
+                        key={player.id}
+                        name={player.name}
+                        description={player.userType}
+                        avatarProps={{
+                            src: player.avatar
+                        }}
+                    />
+                ))}
+            </div>
+            </CardBody>
+            <CardFooter>
+                <form onSubmit={startGame}>
+                    <input type="hidden" name="gameId" value={gameId} />
+                    <Button type="submit">Start Game</Button>
+                </form>
+            </CardFooter>
+        </Card>
     );
 }
