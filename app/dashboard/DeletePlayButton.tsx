507d82d8dab64f0441bf678f606b047d63da5bca
Green Gem <chris_waitt@me.com>
2024-03-19 20:09:40 +0000

    Add DeletePlayButton component

---
+++ app/dashboard/DeletePlayButton.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@nextui-org/button";
+
+export default function DeletePlayButton({
+    playId
+}: {
+    playId: string;
+}) {
+    return (
+        <Button>
+            Delete
+        </Button>
+    )
+}
