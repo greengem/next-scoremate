dab47b43ee71f5b9dc7fdcfd5d1b326bb1ee208f
Green Gem <chris_waitt@me.com>
2024-03-19 17:08:42 +0000

    Update Navbar styling and class names

--- ui/Navbar.tsx
+++ ui/Navbar.tsx
@@ -4,7 +4,7 @@ import { Button } from "@nextui-org/button";
 import { ThemeSwitcher } from "./ThemeSwitcher";
 export default function Navbar() {
     return(
-        <nav className="flex justify-between items-center dark:bg-black px-5 py-3 dark:text-white border-b-2 border-zinc-500">
+        <nav className="dark:bg-ctp-crust flex justify-between items-center px-5 py-3 navbar h-14">
             <h4 className="text-xl tracking-tight"><Link href='/'>Brand</Link></h4>
             <ul className="flex gap-3 items-center">
                 <li><ThemeSwitcher /></li>
