6edc57d94834ac9f0e8ed64117ceab2e539a3eac
Green Gem <chris_waitt@me.com>
2024-03-19 15:05:29 +0000

    Add ThemeSwitcher component

---
+++ ui/ThemeSwitcher.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { IconDeviceDesktop, IconMoonStars, IconSunHigh } from "@tabler/icons-react";
+import {useTheme} from "next-themes";
+import { useEffect, useState } from "react";
+
+export function ThemeSwitcher() {
+  const [mounted, setMounted] = useState(false)
+  const { theme, setTheme } = useTheme()
+
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  if(!mounted) return null
+
+  return (
+    <div className="flex gap-2 items-center">
+      <button onClick={() => setTheme('light')}><IconSunHigh /></button>
+      <button onClick={() => setTheme('dark')}><IconMoonStars /></button>
+      <button onClick={() => setTheme('dark')}><IconDeviceDesktop /></button>
+    </div>
+  )
+};
