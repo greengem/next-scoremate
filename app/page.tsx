e9667f15d2148fe2ec635f95492c4e1f546af192
Green Gem <chris_waitt@me.com>
2024-03-19 15:03:08 +0000

    Add New Game button to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import { auth } from "@clerk/nextjs";
 import PageHeading from "@/ui/PageHeading";
 import prisma from "@/db/prisma";
+import { Button } from "@nextui-org/button";
+import Link from "next/link";
 
 export default async function HomePage() {
   const { userId } = auth();
@@ -17,6 +19,7 @@ export default async function HomePage() {
   return (
     <>
       <PageHeading title="Home" />
+      <Button color="primary" size="sm" as={Link} href='/new-game' className="mb-3">New Game</Button>
       <pre>{JSON.stringify(games, null, 2)}</pre>    
     </>
   );
