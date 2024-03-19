7c11b27014b7d269aa48d46edfd195288ce3a976
Green Gem <chris_waitt@me.com>
2024-03-19 15:05:24 +0000

    Add ThemeSwitcher component to Navbar

--- ui/Navbar.tsx
+++ ui/Navbar.tsx
@@ -1,12 +1,13 @@
 import { UserButton } from "@clerk/nextjs";
 import Link from "next/link";
 import { Button } from "@nextui-org/button";
+import { ThemeSwitcher } from "./ThemeSwitcher";
 export default function Navbar() {
     return(
-        <nav className="flex justify-between items-center bg-black px-5 py-3 text-white">
+        <nav className="flex justify-between items-center dark:bg-black px-5 py-3 dark:text-white border-b-2 border-zinc-500">
             <h4 className="text-xl tracking-tight"><Link href='/'>Brand</Link></h4>
-            <ul className="flex gap-3">
-                <li><Button color="primary" size="sm" as={Link} href='/new-game'>New Game</Button></li>
+            <ul className="flex gap-3 items-center">
+                <li><ThemeSwitcher /></li>
                 <li><UserButton /></li>
             </ul>
         </nav>
