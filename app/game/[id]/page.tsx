5bd9e728f1db632ddcf4a62512a0dd9e3a928815
Green Gem <chris_waitt@me.com>
2024-03-19 17:08:21 +0000

    Refactor player rendering and add score input

--- app/game/[id]/page.tsx
+++ app/game/[id]/page.tsx
@@ -1,7 +1,11 @@
 import prisma from "@/db/prisma"
 import { clerkClient } from "@clerk/nextjs";
+import { Card, CardBody } from "@nextui-org/card";
+import { Input } from "@nextui-org/input";
+import { Button } from "@nextui-org/react";
 
 export default async function Page({ params } : { params: { id: string } }) {
+
   const play = await prisma.play.findUnique({
     where: { id: params.id },
     include: {
@@ -13,34 +17,39 @@ export default async function Page({ params } : { params: { id: string } }) {
     return <div>Game not found</div>;
   }
 
-  const creator = await clerkClient.users.getUser(play.creatorId);
-  const friends = play.players.filter(player => player.userId !== null && player.userId !== play.creatorId);
-  const guests = play.players.filter(player => player.userId === null);
+  const sortedPlayers = [...play.players].sort((a, b) => a.order - b.order);
+
+  const playersData = await Promise.all(sortedPlayers.map(async player => {
+    if (player.userId) {
+      const userData = await clerkClient.users.getUser(player.userId);
+      return { 
+        username: userData.username, 
+        imageUrl: userData.imageUrl, 
+        order: player.order 
+      };
+    } else {
+      return { 
+        username: player.guestName, 
+        imageUrl: player.guestAvatar, 
+        order: player.order 
+      };
+    }
+  }));
 
   return (
-    <div>
-      <pre>{JSON.stringify(play, null, 2)}</pre>
-
-      <h2 className="text-xl mb-3">Creator: {creator.firstName} {creator.lastName}</h2>
-
-      <div className="mb-3">
-        <p className="text-xl">Friends: </p>
-        {friends.map((friend, index) => (
-          <div key={index}>
-            <h3>{friend.guestName}</h3>
-          </div>
+    <form>
+      <div className="grid grid-cols-3 gap-5 mb-5">
+        {sortedPlayers.map((player, index) => (
+          <Card key={player.id} shadow="none" className="bg-ctp-surface0 shadow-lg text-ctp-text">
+            <CardBody className="p-5">
+              <p className="text-center text-xl font-semibold mb-5">{playersData[index].username}</p>
+              <img src={playersData[index].imageUrl ?? undefined} alt={playersData[index].username ?? undefined} className="rounded-full mb-5" />
+              <Input type="number" placeholder={`${playersData[index].username}'s Score`} className="w-full" />
+            </CardBody>
+          </Card>
         ))}
       </div>
-
-      <div className="mb-3">
-        <p className="text-xl">Guests: </p>
-        {guests.map((guest, index) => (
-          <div key={index}>
-            <h3>{guest.guestName}</h3>
-          </div>
-        ))}
-      </div>
-
-    </div>
+      <Button className="bg-ctp-mauve text-ctp-base">Save Game</Button>
+    </form>
   );
 }
