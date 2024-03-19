cf94f59ee27b879639792406340e58b25b01818d
Green Gem <chris_waitt@me.com>
2024-03-19 15:04:54 +0000

    Refactor PlayGamePage component

--- app/new-game/[id]/page.tsx
+++ app/new-game/[id]/page.tsx
@@ -1,13 +1,14 @@
 import { auth, currentUser } from "@clerk/nextjs";
 import fetchBoardGameData from '@/data/FetchBoardGameData';
 import PageHeading from '@/ui/PageHeading';
-import NewGameStartGame from './_StartGame/NewGameStartGame';
+import PlayerSelect from './PlayerSelect';
 
 interface PlayGamePageProps {
     params: { id: string };
 }
 
 export default async function PlayGamePage({ params } : PlayGamePageProps) {
+    // Get user info
     const user = await currentUser()
     if (!user) {
         throw new Error('User not found');
@@ -16,18 +17,20 @@ export default async function PlayGamePage({ params } : PlayGamePageProps) {
     const userName = user?.firstName || 'User';
     const userAvatar = user?.imageUrl || '';
 
+    // Get game info
     const boardGame = await fetchBoardGameData(params.id);
 
+    // Set page title
     const pageTitle = `New Game of ${boardGame.name}`;
 
     return (
         <>
             <PageHeading title={pageTitle} />
-            <NewGameStartGame 
+            <PlayerSelect
                 gameId={params.id}
-                userId={userId}
-                userName={userName}
-                userAvatar={userAvatar}
+                creatorId={userId}
+                creatorName={userName}
+                creatorAvatar={userAvatar}
             />
         </>
     )
