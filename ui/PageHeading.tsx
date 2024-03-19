a1c24e510fff68322dc13ce0e9d8246fded622e7
Green Gem <chris_waitt@me.com>
2024-03-19 20:10:14 +0000

    Update PageHeading font size

--- ui/PageHeading.tsx
+++ ui/PageHeading.tsx
@@ -1,3 +1,3 @@
 export default function PageHeading({ title } : { title: string }) {
-    return <h1 className="text-7xl/tight font-semibold tracking-tight block mb-5">{title}</h1>
+    return <h1 className="text-4xl/tight font-semibold tracking-tight block mb-5">{title}</h1>
 }
