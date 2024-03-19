f866167462d79b388c4d8a8226bbafdd81aaa843
Green Gem <chris_waitt@me.com>
2024-03-19 17:08:09 +0000

    Update NextUIProvider className to include flex flex-col grow

--- app/providers.tsx
+++ app/providers.tsx
@@ -5,7 +5,7 @@ import {ThemeProvider as NextThemesProvider} from "next-themes";
 
 export function Providers({children}: { children: React.ReactNode }) {
   return (
-    <NextUIProvider>
+    <NextUIProvider className="flex flex-col grow">
       <NextThemesProvider attribute="class" defaultTheme="system">
         {children}
       </NextThemesProvider>
