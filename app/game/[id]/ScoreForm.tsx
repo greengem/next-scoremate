1b1b7ba8f956c5dc020bb757c35bd32749d068e0
Green Gem <chris_waitt@me.com>
2024-03-20 17:13:29 +0000

    Update ScoreForm component

--- app/game/[id]/ScoreForm.tsx
+++ app/game/[id]/ScoreForm.tsx
@@ -3,7 +3,8 @@ import { handleSaveScores } from "@/server-actions/GameServerActions";
 import { useState } from "react";
 import { Card, CardBody } from "@nextui-org/card";
 import { Input } from "@nextui-org/input";
-import { Button } from "@nextui-org/react";
+import { Button } from "@nextui-org/button";
+import Image from "next/image";
 
 export default function ScoreForm({
     playersData,
@@ -35,7 +36,7 @@ export default function ScoreForm({
                 <Card key={index} shadow="none" className="bg-ctp-surface0 shadow-lg text-ctp-text">
                 <CardBody className="p-5">
                     <p className="text-center text-xl font-semibold mb-5">{player.username}</p>
-                    <img src={player.imageUrl ?? undefined} alt={player.username ?? undefined} className="rounded-full mb-5" />
+                    <Image src={player.imageUrl ?? undefined} alt={player.username ?? undefined} className="rounded-full mb-5" width={600} height={600} />
                     <Input type="number" placeholder={`${player.username}'s Score`} className="w-full" onChange={e => handleScoreChange(index, Number(e.target.value))} />
                 </CardBody>
                 </Card>
