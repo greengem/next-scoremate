89efc3220a7784f0721f934dc9a1dcc789ef7f28
Green Gem <chris_waitt@me.com>
2024-03-20 17:10:24 +0000

    Refactor PlayGamePage component to include game title and image in PlayerSelect component

--- app/new-game/[id]/page.tsx
+++ app/new-game/[id]/page.tsx
@@ -20,17 +20,24 @@ export default async function PlayGamePage({ params } : PlayGamePageProps) {
     // Get game info
     const boardGame = await fetchBoardGameData(params.id);
 
+    // console.log(boardGame);
+
     // Set page title
     const pageTitle = `New Game of ${boardGame.name}`;
 
     return (
-        <div className="flex flex-col grow items-center justify-center">
-            <PlayerSelect
-                gameId={params.id}
-                creatorId={userId}
-                creatorName={userName}
-                creatorAvatar={userAvatar}
-            />
-        </div>
+        <>
+            <PageHeading title={pageTitle} />
+            <div className="flex flex-col grow items-center justify-center">
+                <PlayerSelect
+                    gameId={params.id}
+                    gameTitle={boardGame.name}
+                    gameImage={boardGame.image}
+                    creatorId={userId}
+                    creatorName={userName}
+                    creatorAvatar={userAvatar}
+                />
+            </div>
+        </>
     )
 }
