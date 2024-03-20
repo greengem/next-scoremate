2e0351ea66fcd23a0236886585210d0ea2254bf3
Green Gem <chris_waitt@me.com>
2024-03-20 17:09:52 +0000

    Remove unused imports and button from HomePage component

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,6 @@
 import { auth } from "@clerk/nextjs";
 import PageHeading from "@/ui/PageHeading";
 import prisma from "@/db/prisma";
-import { Button } from "@nextui-org/button";
-import Link from "next/link";
 
 export default async function HomePage() {
   const { userId } = auth();
@@ -19,7 +17,6 @@ export default async function HomePage() {
   return (
     <>
       <PageHeading title="Home" />
-      <Button color="primary" size="sm" as={Link} href='/new-game' className="mb-3">New Game</Button>
       <pre>{JSON.stringify(games, null, 2)}</pre>    
     </>
   );
