6b61b4cf90c94da5ee86a4c88bea3aaa2386bfa7
Green Gem <chris_waitt@me.com>
2024-03-20 17:10:46 +0000

    Refactor ThemeSwitcher component to include theme options

--- ui/ThemeSwitcher.tsx
+++ ui/ThemeSwitcher.tsx
@@ -1,24 +1,41 @@
 "use client";
 
 import { IconDeviceDesktop, IconMoonStars, IconSunHigh } from "@tabler/icons-react";
-import {useTheme} from "next-themes";
+import { useTheme } from "next-themes";
 import { useEffect, useState } from "react";
+import clsx from "clsx";
 
 export function ThemeSwitcher() {
-  const [mounted, setMounted] = useState(false)
-  const { theme, setTheme } = useTheme()
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme } = useTheme();
 
   useEffect(() => {
-    setMounted(true)
-  }, [])
+    setMounted(true);
+  }, []);
 
-  if(!mounted) return null
+  if (!mounted) return null;
+
+  const themes = [
+    { name: 'light', icon: <IconSunHigh />, label: 'Light theme' },
+    { name: 'dark', icon: <IconMoonStars />, label: 'Dark theme' },
+    { name: 'system', icon: <IconDeviceDesktop />, label: 'System theme' },
+  ];
 
   return (
-    <div className="flex gap-2 items-center">
-      <button onClick={() => setTheme('light')}><IconSunHigh /></button>
-      <button onClick={() => setTheme('dark')}><IconMoonStars /></button>
-      <button onClick={() => setTheme('system')}><IconDeviceDesktop /></button>
-    </div>
-  )
+    <ul className="flex flex-col gap-2 justify-center items center">
+      {themes.map(({ name, icon, label }) => (
+        <li key={name} className="flex justify-center">
+          <button 
+            aria-label={label}
+            onClick={() => setTheme(name)} 
+            className={clsx("dark:hover:bg-ctp-mantle dark:hover:text-ctp-mauve hover:bg-zinc-200 p-2 rounded-full", {
+              'text-black dark:text-ctp-mauve bg-primary dark:bg-ctp-mantle ring-1 ring-ctp-mauve': theme === name
+            })}
+          >
+            {icon}
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
 };
