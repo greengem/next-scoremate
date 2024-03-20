dde9df0a73e121341a48bfc95ba9c2e901f9e939
Green Gem <chris_waitt@me.com>
2024-03-20 17:08:49 +0000

    update configuration to tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,7 +13,34 @@ const config: Config = {
   },
   darkMode: "class",
   plugins: [
-    nextui(),
+    nextui({
+      themes: {
+        light: {
+          colors: {
+            background: "#FFFFFF",
+            foreground: "#000000",
+
+            primary: {
+              500: "#c6a0f6",
+              foreground: "#000000",
+              DEFAULT: "#c6a0f6",
+            },
+          }
+        },
+        dark: {
+          colors: {
+            background: "#FFFFFF",
+            foreground: "#cad3f5",
+
+            primary: {
+              500: "#c6a0f6",
+              foreground: "#000000",
+              DEFAULT: "#c6a0f6",
+            },
+          }
+        },
+      }
+    }),
     require("@catppuccin/tailwindcss")({
       prefix: "ctp",
       defaultFlavour: "macchiato",
