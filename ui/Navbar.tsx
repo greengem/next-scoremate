ea7806d2577ec8ffe3eeecd653a5484da7c2d6e1
Green Gem <chris_waitt@me.com>
2024-03-20 17:10:42 +0000

    Update Navbar styling and remove ThemeSwitcher

--- ui/Navbar.tsx
+++ ui/Navbar.tsx
@@ -1,13 +1,11 @@
 import { UserButton } from "@clerk/nextjs";
 import Link from "next/link";
-import { Button } from "@nextui-org/button";
-import { ThemeSwitcher } from "./ThemeSwitcher";
+
 export default function Navbar() {
     return(
-        <nav className="dark:bg-ctp-crust flex justify-between items-center px-5 py-3 navbar h-14">
+        <nav className="fixed top-0 left-0 right-0 z-10 dark:bg-ctp-crust flex justify-between items-center px-5 py-3 navbar h-14">
             <h4 className="text-xl tracking-tight"><Link href='/'>Brand</Link></h4>
             <ul className="flex gap-3 items-center">
-                <li><ThemeSwitcher /></li>
                 <li><UserButton /></li>
             </ul>
         </nav>
