c8160b2159df7abbfc2f69605fd9d7d323d5bbb2
Green Gem <chris_waitt@me.com>
2024-03-21 12:39:47 +0000

    Refactor ScoreForm component to use User and CustomInput components

--- app/game/[id]/ScoreForm.tsx
+++ app/game/[id]/ScoreForm.tsx
@@ -1,10 +1,12 @@
 'use client'
 import { handleSaveScores } from "@/server-actions/GameServerActions";
 import { useState } from "react";
-import { Card, CardBody } from "@nextui-org/card";
+import { Card, CardBody, CardHeader } from "@nextui-org/card";
 import { Input } from "@nextui-org/input";
 import { Button } from "@nextui-org/button";
 import Image from "next/image";
+import { User } from "@nextui-org/user";
+import CustomInput from "@/ui/CustomNextUI/CustomInput";
 
 export default function ScoreForm({
     playersData,
@@ -31,17 +33,31 @@ export default function ScoreForm({
 
     return (
         <form onSubmit={handleSubmit}>
-            <div className="grid grid-cols-3 gap-5 mb-5">
-            {playersData.map((player, index) => (
-                <Card key={index} shadow="none" className="bg-ctp-surface0 shadow-lg text-ctp-text">
-                <CardBody className="p-5">
-                    <p className="text-center text-xl font-semibold mb-5">{player.username}</p>
-                    <Image src={player.imageUrl ?? undefined} alt={player.username ?? undefined} className="rounded-full mb-5" width={600} height={600} />
-                    <Input type="number" placeholder={`${player.username}'s Score`} className="w-full" onChange={e => handleScoreChange(index, Number(e.target.value))} />
-                </CardBody>
-                </Card>
-            ))}
+
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 mb-5">
+                {playersData.map((player, index) => (
+                    <Card key={index} shadow="none" className="bg-ctp-surface0 shadow-lg text-ctp-text">
+                        <CardHeader>
+                            <User   
+                                name={player.username}
+                                description="Product Designer"
+                                avatarProps={{
+                                    src: player.imageUrl ?? undefined,
+                                }}
+                            />
+                        </CardHeader>
+                        <CardBody>
+                            <CustomInput 
+                                type="number"
+                                label="Score" placeholder={`${player.username}'s Score`}
+                                className="w-full"
+                                onChange={e => handleScoreChange(index, Number(e.target.value))}
+                            />
+                        </CardBody>
+                    </Card>
+                ))}
             </div>
+
             <Button color="primary" type="submit">
                 Finish Game
             </Button>
